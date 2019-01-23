7849388f3b38fc4fe2df92d01af03b0caa355e2d
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2019-01-23 17:15:04 -0500

    Added models and controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,19 @@
 const path = require('path');
+
 const express = require('express');
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
